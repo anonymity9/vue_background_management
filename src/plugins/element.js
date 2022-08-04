aa0081fecf416bc21781fdbd7507e402cd50da5e
farewell2 <15086230695@163.com>
2022-08-04 21:59:03 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// import { Button } from 'element-ui'
+// import { Form, FormItem } from 'element-ui'
+// import { input } from 'element-ui' 
+// // 导入弹框组件
+// import { Message } from 'element-ui'
+
+import { Button, Message, Form, FormItem, input} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(input)
+Vue.prototype.$message = Message

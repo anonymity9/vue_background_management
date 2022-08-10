2f8da420faa8b85dd94a448cc9608fac23eeb556
farewell2 <15086230695@163.com>
2022-08-11 01:27:59 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 // 导入全局样式表
 import '../assets/css/global.css'
@@ -9,9 +11,17 @@ import '../assets/css/global.css'
 Vue.use(VueRouter)
 
 const routes = [
+
   {path: '/',redirect: '/login' },
   {path:'/login',component:Login },
-  {path:'/home',component:Home}
+  {path:'/home',
+   component:Home,
+   redirect:'/welcome',
+  children: [{ path: '/welcome', component: Welcome },
+             { path: '/users', component: Users }
+           ]
+  }
+
 ]
 
  const router = new VueRouter({
